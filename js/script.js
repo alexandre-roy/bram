98b8e1f244760469a8d5c299c3d8ae4679b60247
Alexandre Roy <alexandre15roy@gmail.com>
2025-09-25 17:57:39 -0400

    [FEAT] - animation du h2 et favicon

--- js/script.js
+++ js/script.js
@@ -27,3 +27,28 @@ function initialisation() {
 window.addEventListener("DOMContentLoaded", initialisation);
 
 const nav = document.getElementById('main-nav');
+
+const dot = document.querySelector(".dot");
+const section = document.querySelector(".s1");
+const heading = section.querySelector("h2");
+
+function checkDotCollision() {
+  const dotRect = dot.getBoundingClientRect();
+  const sectionRect = heading.getBoundingClientRect();
+
+  if (dotRect.bottom >= sectionRect.top && dotRect.top <= sectionRect.bottom) {
+    dot.classList.add("active");
+  } else {
+    dot.classList.remove("active");
+  }
+
+  if (dot.classList.contains("active")) {
+    heading.classList.add("show");
+  } else {
+    heading.classList.remove("show");
+  }
+
+  requestAnimationFrame(checkDotCollision);
+}
+
+checkDotCollision();
