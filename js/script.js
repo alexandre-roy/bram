ce887c36f6db3e9430e3598d455bbf9bd054b132
darkous777 <166870169+darkous777@users.noreply.github.com>
2025-09-26 22:51:26 -0400

    [CHORE] ajout du mail

--- js/script.js
+++ js/script.js
@@ -4,28 +4,6 @@
 "use strict";
 
 
-/*************
-    Cette fonction est rattachée à l'événement "Load".
-    C'est la première fonction qui va s'executer lorsque
-    la page sera entièrement chargée.
-**************/
-/**
- * Fonction de depart et gestionnaire d'evenements
- */
-function initialisation() {
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      if (target) {
-        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
-      }
-    });
-  });
-}
-
-window.addEventListener("DOMContentLoaded", initialisation);
-
 const nav = document.getElementById('main-nav');
 
 const dot = document.querySelector(".dot");
@@ -52,3 +30,24 @@ function checkDotCollision() {
 }
 
 checkDotCollision();
+
+/*************
+    Cette fonction est rattachée à l'événement "Load".
+    C'est la première fonction qui va s'executer lorsque
+    la page sera entièrement chargée.
+**************/
+/**
+ * Fonction de depart et gestionnaire d'evenements
+ */
+function initialisation() {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    });
+  });
+}
+window.addEventListener("DOMContentLoaded", initialisation);
